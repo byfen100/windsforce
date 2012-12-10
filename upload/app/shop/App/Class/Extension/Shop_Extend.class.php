6f33cb00f2fc372f1c9cfcbfc5215d8c74850de5
liuxiangmin <log1990@126.com>
2012-12-10 22:59:36 +0800

    去掉多余的东西，开始精简，清理数据库记录

--- upload/app/shop/App/Class/Extension/Shop_Extend.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* [WindsForce!] (C)WindsForce Studio start this From 2012.03.17.
-   商城核心函数库文件($)*/
-
-!defined('DYHB_PATH') && exit;
-
-class Shop_Extend{
-
-}
--- upload/app/shop/install.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* [WindsForce!] (C)WindsForce Studio start this From 2012.03.17.
-   商城初始化安装程序($)*/
-
-!defined('DYHB_PATH') && exit;
-
-$bFinish=TRUE;
