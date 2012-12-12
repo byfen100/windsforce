f88e27d70e5cab2b684161daf8a8ad70f8fe388d
liuxiangmin <log1990@126.com>
2012-12-13 02:10:42 +0800

    home应用控制器语言包搞定

--- upload/app/home/App/Lang/Zh-tw/Controller/Space.lang.php
+++ upload/app/home/App/Lang/Zh-tw/Controller/Space.lang.php
@@ -2,6 +2,9 @@
 /** DoYouHaoBaby Framework Lang File, Do not to modify it! */
 return array(
 '7c9f756bc693f91af8e70851bd20886f'=>"你指定的用戶不存在",
+'9f68a8079815033b88fcc6601abd6642'=>"積分",
+'42ddc62228bde29a075f2de4837c9f87'=>"個人空間",
+'882a6aab0478d99d37e7b724792c7cb3'=>"我的頭像",
 )
 
 ?>
---
+++ upload/app/home/App/Lang/Zh-tw/Controller/Stat.lang.php
@@ -0,0 +1,8 @@
+<?php
+/** DoYouHaoBaby Framework Lang File, Do not to modify it! */
+return array(
+'5386bb191583a12597b2ea7daef29b4a'=>"會員列表",
+'7b816f3f172a8c945a66053fe10fc3a4'=>"基本概況",
+)
+
+?>
