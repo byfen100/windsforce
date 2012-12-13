c5d4a67bc684db41b528e1d8d3207cf60034240f
liuxiangmin <log1990@126.com>
2012-12-13 22:56:51 +0800

    提炼杂项和短消息语言包

---
+++ upload/app/home/App/Lang/Zh-cn/Template/Misc.lang.php
@@ -0,0 +1,18 @@
+<?php
+/** DoYouHaoBaby Framework Lang File, Do not to modify it! */
+return array(
+'2c56626abca29c4755b55fa11a5db875'=>"前台皮肤测试效果",
+'21a10ba1cce7b1a92b661cfc159d86a7'=>"未设置现居地",
+'7c0c2cd1a1a64fa01a68e3e1f705ef60'=>"该用户还没有设置签名",
+'4c0a3ab48ef1af7edb8e77496363d457'=>"关注",
+'1c173c837882e4a435755601517b93c6'=>"粉丝",
+'9f68a8079815033b88fcc6601abd6642'=>"积分",
+'7402599bc6350c095fb3ca545d950480'=>"金币",
+'92bdc8b15dd72f5df7343d79c7d5b1c2'=>"取消关注",
+'fc339dffc9ab9599d081d207bd361de7'=>"加关注",
+'749e5f666ba2285cf0037bdb8f3675fd'=>"写私信",
+'048fa1f2fbaf1676c1acb488fea52274'=>"请填写一个视频地址,支持swf,asf,wmv,avi,rm,rmvb,flv,mp4格式",
+'287271c2c5cdb5b3f5e3c2f0da6825ef'=>"请填写一个音乐地址,支持mp3,wma,wav格式",
+)
+
+?>
