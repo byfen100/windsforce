2dd9f2b1ea93e588bf8b233d2ac71436635d2e99
liuxiangmin <log1990@126.com>
2012-12-14 00:52:19 +0800

    home应用语言包全部提炼完毕，现在准备开始提炼全局语言包

---
+++ upload/app/home/App/Lang/Zh-tw/Template/Misc.lang.php
@@ -0,0 +1,18 @@
+<?php
+/** DoYouHaoBaby Framework Lang File, Do not to modify it! */
+return array(
+'2c56626abca29c4755b55fa11a5db875'=>"前台皮膚測試效果",
+'21a10ba1cce7b1a92b661cfc159d86a7'=>"未設置現居地",
+'7c0c2cd1a1a64fa01a68e3e1f705ef60'=>"該用戶還沒有設置簽名",
+'4c0a3ab48ef1af7edb8e77496363d457'=>"關注",
+'1c173c837882e4a435755601517b93c6'=>"粉絲",
+'9f68a8079815033b88fcc6601abd6642'=>"積分",
+'7402599bc6350c095fb3ca545d950480'=>"金幣",
+'92bdc8b15dd72f5df7343d79c7d5b1c2'=>"取消關注",
+'fc339dffc9ab9599d081d207bd361de7'=>"加關注",
+'749e5f666ba2285cf0037bdb8f3675fd'=>"寫私信",
+'048fa1f2fbaf1676c1acb488fea52274'=>"請填寫一個視頻地址,支持swf,asf,wmv,avi,rm,rmvb,flv,mp4格式",
+'287271c2c5cdb5b3f5e3c2f0da6825ef'=>"請填寫一個音樂地址,支持mp3,wma,wav格式",
+)
+
+?>
