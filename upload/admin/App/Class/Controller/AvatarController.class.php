61aed9a4ce4e70b6fa21323c1a587019b5f01b0d
liuxiangmin <log1990@126.com>
2013-01-16 22:10:49 +0800

    上传了头像，更新我的头像状态

--- upload/admin/App/Class/Controller/AvatarController.class.php
+++ upload/admin/App/Class/Controller/AvatarController.class.php
@@ -48,8 +48,18 @@ class AvatarController extends InitController{
 			$this->E(Dyhb::L('你的PHP 版本或者配置中不支持如下的函数 “imagecreatetruecolor”、“imagecopyresampled”等图像函数，所以创建不了头像','Controller/Avatar'));
 		}
 
-		$this->assign('__JumpUrl__',Dyhb::U('avatar/index'));
+		// 更新是否上传头像
+		$oUser=UserModel::F('user_id=?',$GLOBALS['___login___']['user_id'])->getOne();
+		if(!empty($oUser['user_id'])){
+			$oUser->user_avatar='1';
+			$oUser->save(0,'update');
 
+			if($oUser->isError()){
+				$this->E($oUser->getErrorMessage());
+			}
+		}
+
+		$this->assign('__JumpUrl__',Dyhb::U('avatar/index'));
 		$this->S(Dyhb::L('头像上传成功','Controller/Avatar'));
 	}
 
--- upload/app/home/App/Class/Controller/Spaceadmin_/Avatar/SavecropController_.php
+++ upload/app/home/App/Class/Controller/Spaceadmin_/Avatar/SavecropController_.php
@@ -13,8 +13,18 @@ class SavecropController extends Controller{
 			$this->E(Dyhb::L('你的PHP 版本或者配置中不支持如下的函数 “imagecreatetruecolor”、“imagecopyresampled”等图像函数，所以创建不了头像','Controller/Spaceadmin'));
 		}
 
-		$this->assign('__JumpUrl__',Dyhb::U('spaceadmin/avatar'));
+		// 更新是否上传头像
+		$oUser=UserModel::F('user_id=?',$GLOBALS['___login___']['user_id'])->getOne();
+		if(!empty($oUser['user_id'])){
+			$oUser->user_avatar='1';
+			$oUser->save(0,'update');
 
+			if($oUser->isError()){
+				$this->E($oUser->getErrorMessage());
+			}
+		}
+
+		$this->assign('__JumpUrl__',Dyhb::U('spaceadmin/avatar'));
 		$this->S(Dyhb::L('头像上传成功','Controller/Spaceadmin'));
 	}
 
