2c73f5a85a6eaaffeff57b65e8e5bb2e892de6f6
liuxiangmin <log1990@126.com>
2013-01-26 20:27:13 +0800

    优化安装程序界面

--- upload/install/App/~Runtime/Cache/Default/common_footer.html.compiled.php
+++ upload/install/App/~Runtime/Cache/Default/common_footer.html.compiled.php
@@ -1,2 +1,2 @@
-<?php  /* DoYouHaoBaby Framework 模板缓存文件生成时间：2012-12-30 09:01:31  */ ?>
+<?php  /* DoYouHaoBaby Framework 模板缓存文件生成时间：2013-01-26 20:23:22  */ ?>
 <hr><footer><p>&copy;2012 <a href="http://windsforce.com/">WindsForce.COM</a> All rights reserved.</p></footer></div></body></html>
