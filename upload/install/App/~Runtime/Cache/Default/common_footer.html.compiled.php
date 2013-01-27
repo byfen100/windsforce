bd1b17de3ba430e89a8389a64085cfb6754c661d
liuxiangmin <log1990@126.com>
2013-01-27 13:07:53 +0800

    更新安装程序底部时间为2012-2013

--- upload/install/App/~Runtime/Cache/Default/common_footer.html.compiled.php
+++ upload/install/App/~Runtime/Cache/Default/common_footer.html.compiled.php
@@ -1,2 +1,2 @@
-<?php  /* DoYouHaoBaby Framework 模板缓存文件生成时间：2013-01-26 20:23:22  */ ?>
-<hr><footer><p>&copy;2012 <a href="http://windsforce.com/">WindsForce.COM</a> All rights reserved.</p></footer></div></body></html>
+<?php  /* DoYouHaoBaby Framework 模板缓存文件生成时间：2013-01-27 13:05:12  */ ?>
+<hr><footer><p>&copy;2012-2013 <a href="http://windsforce.com/">WindsForce.COM</a> All rights reserved.</p></footer></div></body></html>
