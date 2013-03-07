40589825e01206810da635252f7d8fce21064db7
liuxiangmin <log1990@126.com>
2013-03-07 10:49:22 +0800

    提醒全部搞定，下一步语言包补齐

--- upload/admin/App/Class/Controller/NoticeController.class.php
+++ upload/admin/App/Class/Controller/NoticeController.class.php
@@ -14,4 +14,50 @@ class NoticeController extends InitController{
 		}
 	}
 
+	public function show(){
+		$nId=G::getGpc('id','G');
+
+		if(!empty($nId)){
+			$oModel=NoticeModel::F('notice_id=?',$nId)->query();
+
+			if(!empty($oModel->notice_id)){
+				$arrData=@unserialize($oModel['notice_data']);
+		
+				$arrTempdata=array();
+				if(is_array($arrData)){
+					foreach($arrData as $nK=>$sValueTemp){
+						$sTempkey='{'.$nK.'}';
+
+						// @开头表示URL，调用Dyhb::U来生成地址
+						if(strpos($nK,'@')===0){
+							$sValueTemp='Dyhb::U('.$sValueTemp.')';
+							$sValueTemp='javascript:alert(\''.$sValueTemp.'\');';
+						}
+
+						$arrTempdata[$sTempkey]=$sValueTemp;
+					}
+				}
+
+				$arrNoticedata=array(
+					'user_id'=>$oModel['notice_authorid'],
+					'notice_username'=>$oModel['notice_authorusername'],
+					'notice_content'=>strtr($oModel['notice_template'],$arrTempdata),
+					'create_dateline'=>$oModel['notice_fromnum']>1?$oModel['update_dateline']:$oModel['create_dateline'],
+					'notice_fromnum'=>$oModel['notice_fromnum'],
+					'notice_type'=>$oModel['notice_type'],
+				);
+
+				$this->assign('oValue',$oModel);
+				$this->assign('nId',$nId);
+				$this->assign('arrNoticedata',$arrNoticedata);
+				
+				$this->display('notice+show');
+			}else{
+				$this->E(Dyhb::L('数据库中并不存在该项，或许它已经被删除','Controller/Common'));
+			}
+		}else{
+			$this->E(Dyhb::L('操作项不存在','Controller/Common'));
+		}
+	}
+
 }
