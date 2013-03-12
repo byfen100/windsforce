c06abe2d712bfcd84d6832b26de6f72db6f4af9c
liuxiangmin <log1990@126.com>
2013-03-12 11:23:42 +0800

    调正一下应用缓存函数位置

--- upload/source/function/Cache_Extend.class.php
+++ upload/source/function/Cache_Extend.class.php
@@ -43,10 +43,50 @@ class Cache_Extend{
 					if(strpos($sCache,'_')!==false){
 						$arrCaches=explode('_',$sCache);
 
-						Core_Extend::appUpdateCache(strtolower($arrCaches[1]),strtolower($arrCaches[0]),$arrNotallowed);
+						self::appUpdateCache(strtolower($arrCaches[1]),strtolower($arrCaches[0]),$arrNotallowed);
 					}else{
 						Dyhb::E('cache parameter is error');
 					}
+				}
+			}
+		}
+	}
+
+	public static function appUpdateCache($sCacheName='',$sApp='home',$arrNotallowed=array()){
+		$arrUpdateList=empty($sCacheName)?array():(is_array($sCacheName)?$sCacheName:array($sCacheName));
+
+		if(!$arrUpdateList){
+			$arrUpdatecache=array();
+
+			$arrAllCachefile=G::listDir(WINDSFORCE_PATH.'/app/'.$sApp.'/App/Class/Extension/cache',false,true);
+			foreach($arrAllCachefile as $sCachefile){
+				$sCachefile=strtolower(subStr($sCachefile,14,-5));
+				if(!in_array($sCachefile,$arrNotallowed)){
+					$arrUpdatecache[]=$sCachefile;
+				}
+			}
+
+			foreach($arrUpdatecache as $sUpdatecache){
+				self::appUpdateCache($sUpdatecache);
+			}
+		}else{
+			foreach($arrUpdateList as $sCache){
+				$sCachefile=WINDSFORCE_PATH.'/app/'.$sApp.'/App/Class/Extension/cache/AppUpdateCache'.ucfirst($sCache).'_.php';
+
+				if(is_file($sCachefile)){
+					$sCacheclass='AppUpdateCache'.ucfirst($sCache);
+					if(!Dyhb::classExists($sCacheclass)){
+						require_once(Core_Extend::includeFile('cache/'.$sCacheclass,$sApp,'_.php'));
+					}
+
+					$Callback=array($sCacheclass,'cache');
+					if(is_callable($Callback)){
+						call_user_func($Callback);
+					}else{
+						Dyhb::E('$Callback is not a callback');
+					}
+				}else{
+					Dyhb::E('$sCachefile %s is not exists');
 				}
 			}
 		}
