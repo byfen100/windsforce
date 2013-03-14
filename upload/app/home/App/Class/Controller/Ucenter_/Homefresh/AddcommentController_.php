a9390509b9a41cddcbc6dd310edb9505ddf7119f
liuxiangmin <log1990@126.com>
2013-03-14 13:39:23 +0800

    整理通用评论代码

--- upload/app/home/App/Class/Controller/Ucenter_/Homefresh/AddcommentController_.php
+++ upload/app/home/App/Class/Controller/Ucenter_/Homefresh/AddcommentController_.php
@@ -19,7 +19,7 @@ class AddcommentController extends GlobalchildController{
 		$arrOptions=$GLOBALS['_cache_']['home_option'];
 
 		if($arrOptions['close_comment_feature']==1){
-			$this->E(Dyhb::L('系统关闭了评论功能','Controller/Homefresh'));
+			$this->E(Dyhb::L('系统关闭了评论功能','__COMMON_LANG__@Function/Comment_Extend'));
 		}
 
 		if($arrOptions['seccode_comment_status']==1){
@@ -29,29 +29,29 @@ class AddcommentController extends GlobalchildController{
 		// IP禁止功能
 		$sOnlineip=G::getIp();
 		if(!Comment_Extend::banIp($sOnlineip)){
-			$this->E(Dyhb::L('您的IP %s 已经被系统禁止发表评论','Controller/Homefresh',null,$sOnlineip));
+			$this->E(Dyhb::L('您的IP %s 已经被系统禁止发表评论','__COMMON_LANG__@Function/Comment_Extend',null,$sOnlineip));
 		}
 
 		// 评论名字检测
 		$sCommentName=trim(G::getGpc('homefreshcomment_name'));
 		if(empty($sCommentName)){
-			$this->E(Dyhb::L('评论名字不能为空','Controller/Homefresh'));
+			$this->E(Dyhb::L('评论名字不能为空','__COMMON_LANG__@Function/Comment_Extend'));
 		}
 
 		if(!Comment_Extend::commentName($sCommentName)){
-			$this->E(Dyhb::L('此评论名字包含不可接受字符或被管理员屏蔽,请选择其它名字','Controller/Homefresh'));
+			$this->E(Dyhb::L('此评论名字包含不可接受字符或被管理员屏蔽,请选择其它名字','__COMMON_LANG__@Function/Comment_Extend'));
 		}
 
 		// 评论内容长度检测
 		$sCommentContent=trim(G::getGpc('homefreshcomment_content'));
 		$nCommentMinLen=intval($arrOptions['comment_min_len']);
 		if(!Comment_Extend::commentMinLen($sCommentContent)){
-			$this->E(Dyhb::L('评论内容最少的字节数为 %d','Controller/Homefresh',null,$nCommentMinLen));
+			$this->E(Dyhb::L('评论内容最少的字节数为 %d','__COMMON_LANG__@Function/Comment_Extend',null,$nCommentMinLen));
 		}
 
 		$nCommentMaxLen=intval($arrOptions['comment_max_len']);
 		if(!Comment_Extend::commentMaxLen($sCommentContent)){
-			$this->E(Dyhb::L('评论内容最大的字节数为 %d','Controller/Homefresh',null,$nCommentMaxLen));
+			$this->E(Dyhb::L('评论内容最大的字节数为 %d','__COMMON_LANG__@Function/Comment_Extend',null,$nCommentMaxLen));
 		}
 
 		// 创建评论模型
@@ -61,7 +61,7 @@ class AddcommentController extends GlobalchildController{
 		$result=Comment_Extend::commentSpamUrl($sCommentContent);
 		if($result===false){
 			$nCommentSpamUrlNum=intval($arrOptions['comment_spam_url_num']);
-			$this->E(Dyhb::L('评论内容中出现的衔接数量超过了系统的限制 %d 条','Controller/Homefresh',null,$nCommentSpamUrlNum));
+			$this->E(Dyhb::L('评论内容中出现的衔接数量超过了系统的限制 %d 条','__COMMON_LANG__@Function/Comment_Extend',null,$nCommentSpamUrlNum));
 		}
 		if($result===0){
 			$oHomefreshcomment->homefreshcomment_status=0;
@@ -71,7 +71,7 @@ class AddcommentController extends GlobalchildController{
 		$result=Comment_Extend::commentSpamWords($sCommentContent);
 		if($result===false){
 			if(is_array($result)){
-				$this->E(Dyhb::L("你的评论内容包含系统屏蔽的词语%s",'Controller/Homefresh',null,$result[1]));
+				$this->E(Dyhb::L("你的评论内容包含系统屏蔽的词语%s",'__COMMON_LANG__@Function/Comment_Extend',null,$result[1]));
 			}
 		}
 		if($result===0){
@@ -82,7 +82,7 @@ class AddcommentController extends GlobalchildController{
 		$result=Comment_Extend::commentSpamContentsize($sCommentContent);
 		if($result===false){
 			$nCommentSpamContentSize=intval($arrOptions['comment_spam_content_size']);
-			$this->E(Dyhb::L('评论内容最大的字节数为%d','Controller/Homefresh',null,$nCommentSpamContentSize));
+			$this->E(Dyhb::L('评论内容最大的字节数为%d','__COMMON_LANG__@Function/Comment_Extend',null,$nCommentSpamContentSize));
 		}
 		if($result===0){
 			$oHomefreshcomment->homefreshcomment_status=0;
@@ -96,7 +96,7 @@ class AddcommentController extends GlobalchildController{
 			if(!empty($oUserLasthomefreshcomment['homefreshcomment_id'])){
 				$nLastPostTime=$oUserLasthomefreshcomment['create_dateline'];
 				if(!Comment_Extend::commentSpamPostSpace($nLastPostTime)){
-					$this->E(Dyhb::L('为防止灌水,发表评论时间间隔为 %d 秒','Controller/Homefresh',null,$nCommentPostSpace));
+					$this->E(Dyhb::L('为防止灌水,发表评论时间间隔为 %d 秒','__COMMON_LANG__@Function/Comment_Extend',null,$nCommentPostSpace));
 				}
 			}
 		}
@@ -106,14 +106,14 @@ class AddcommentController extends GlobalchildController{
 			$nCurrentTimeStamp=CURRENT_TIMESTAMP;
 			$oTryComment=HomefreshcommentModel::F("homefreshcomment_name=? AND homefreshcomment_content=? AND {$nCurrentTimeStamp}-create_dateline<86400 AND homefreshcomment_ip=?",$sCommentName,$sCommentContent,$sOnlineip)->order('homefreshcomment_id DESC')->query();
 			if(!empty($oTryComment['homefreshcomment_id'])){
-				$this->E(Dyhb::L('你提交的评论已经存在,24小时之内不允许出现相同的评论','Controller/Homefresh'));
+				$this->E(Dyhb::L('你提交的评论已经存在,24小时之内不允许出现相同的评论','__COMMON_LANG__@Function/Comment_Extend'));
 			}
 		}
 
 		// 纯英文评论阻止
 		$result=Comment_Extend::commentDisallowedallenglishword($sCommentContent);
 		if($result===false){
-			$this->E('You should type some Chinese word(like 你好)in your comment to pass the spam-check, thanks for your patience! '.Dyhb::L('您的评论中必须包含汉字','Controller/Homefresh'));
+			$this->E('You should type some Chinese word(like 你好)in your comment to pass the spam-check, thanks for your patience! '.Dyhb::L('您的评论中必须包含汉字','__COMMON_LANG__@Function/Comment_Extend'));
 		}
 		if($result===0){
 			$oHomefreshcomment->homefreshcomment_status=0;
@@ -133,7 +133,7 @@ class AddcommentController extends GlobalchildController{
 			$this->E($oHomefreshcomment->getErrorMessage());
 		}else{
 			// 发送COOKIE
-			$this->send_cookie($oHomefreshcomment);
+			Comment_Extend::sendCookie($oHomefreshcomment->homefreshcomment_name,$oHomefreshcomment->homefreshcomment_url,$oHomefreshcomment->homefreshcomment_email);
 
 			// 更新评论数量
 			$oHomefreshTemp=Dyhb::instance('HomefreshModel');
@@ -144,44 +144,65 @@ class AddcommentController extends GlobalchildController{
 			}
 			unset($oHomefreshTemp);
 
-			// 发送feed
+			// 读取新鲜事数据
 			$oHomefresh=HomefreshModel::F('homefresh_id=?',intval(G::getGpc('homefresh_id')))->getOne();
 
-			$sFeedtemplate='<div class="feed_addhomefresh"><span class="feed_title">'.Dyhb::L('评论了新鲜事','Controller/Homefresh').'&nbsp;<a href="{@homefresh_commentlink}">{homefresh_title}</a></span><div class="feed_content"><div class="feed_quote"><span class="feed_quoteinfo">{homefresh_commentmessage}</span></div></div><div class="feed_action"><a href="{@homefresh_commentlink}">'.Dyhb::L('回复','Controller/Homefresh').'</a></div></div>';
-
-			$arrFeeddata=array(
-				'@homefresh_commentlink'=>'home://fresh@?id='.$oHomefresh['homefresh_id'].'&isolation_commentid='.$oHomefreshcomment['homefreshcomment_id'],
-				'homefresh_title'=>$oHomefresh['homefresh_title']?G::subString($oHomefresh['homefresh_title'],0,30):G::subString(strip_tags($oHomefresh['homefresh_message']),0,30),
-				'homefresh_commentmessage'=>G::subString(strip_tags($oHomefreshcomment['homefreshcomment_content']),0,100),
-			);
-
-			try{
-				Core_Extend::addFeed($sFeedtemplate,$arrFeeddata);
-			}catch(Exception $e){
-				$this->E($e->getMessage());
-			}
-
-			// 发送提醒
-			if($oHomefresh['user_id']!=$GLOBALS['___login___']['user_id']){
-				$sNoticetemplate='<div class="notice_addhomefreshcomment"><span class="notice_title"><a href="{@space_link}">{user_name}</a>&nbsp;'.Dyhb::L('评论了新鲜事','Controller/Homefresh').'&nbsp;<a href="{@homefresh_commentlink}">{homefresh_title}</a></span><div class="notice_content"><div class="notice_quote"><span class="notice_quoteinfo">{homefresh_commentmessage}</span></div></div><div class="notice_action"><a href="{@homefresh_commentlink}">'.Dyhb::L('查看','Controller/Homefresh').'</a></div></div>';
-
-				$arrNoticedata=array(
-					'@space_link'=>'home://space@?id='.$GLOBALS['___login___']['user_id'],
-					'user_name'=>$GLOBALS['___login___']['user_name'],
-					'@homefresh_commentlink'=>'home://fresh@?id='.$oHomefresh['homefresh_id'].'&isolation_commentid='.$oHomefreshcomment['homefreshcomment_id'],
-					'homefresh_title'=>$oHomefresh['homefresh_title']?G::subString($oHomefresh['homefresh_title'],0,30):G::subString(strip_tags($oHomefresh['homefresh_message']),0,30),
-					'homefresh_commentmessage'=>G::subString(strip_tags($oHomefreshcomment['homefreshcomment_content']),0,100),
-				);
+			if(!empty($oHomefresh['homefresh_id'])){
+				// 发送feed
+				$sCommentLink='home://fresh@?id='.$oHomefresh['homefresh_id'].'&isolation_commentid='.$oHomefreshcomment['homefreshcomment_id'];
+				$sCommentTitle=$oHomefresh['homefresh_title']?$oHomefresh['homefresh_title']:strip_tags($oHomefresh['homefresh_message']);
+				$sCommentMessage=strip_tags($oHomefreshcomment['homefreshcomment_content']);
 
 				try{
-					Core_Extend::addNotice($sNoticetemplate,$arrNoticedata,$oHomefresh['user_id'],'homefreshcomment',$oHomefresh['homefresh_id']);
+					Comment_Extend::addFeed(Dyhb::L('评论了新鲜事','Controller/Homefresh'),'addhomefresh',$sCommentLink,$sCommentTitle,$sCommentMessage);
 				}catch(Exception $e){
 					$this->E($e->getMessage());
 				}
+
+				// 发送提醒
+				if($oHomefresh['user_id']!=$GLOBALS['___login___']['user_id']){
+					$sCommentLink='home://fresh@?id='.$oHomefresh['homefresh_id'].'&isolation_commentid='.$oHomefreshcomment['homefreshcomment_id'];
+					$sCommentTitle=$oHomefresh['homefresh_title']?$oHomefresh['homefresh_title']:strip_tags($oHomefresh['homefresh_message']);
+					$sCommentMessage=strip_tags($oHomefreshcomment['homefreshcomment_content']);
+
+					try{
+						Comment_Extend::addNotice(Dyhb::L('评论了新鲜事','Controller/Homefresh'),'addhomefresh',$sCommentLink,$sCommentTitle,$sCommentMessage,$oHomefresh['user_id'],'homefreshcomment',$oHomefresh['homefresh_id']);
+					}catch(Exception $e){
+						$this->E($e->getMessage());
+					}
+				}
 			}
-			
+
 			// 邮件通知
-			$this->comment_sendmail($oHomefreshcomment);
+			try{
+				$sCommentLink=$GLOBALS['_option_']['site_url'].'/index.php?app=home&c=ucenter&a=view&id='.$oHomefreshcomment->homefresh_id.'&isolation_commentid='.$oHomefreshcomment['homefreshcomment_id'];
+
+				$bHaveParentcomment=false;
+				if($oHomefreshcomment->homefreshcomment_parentid==0){
+					$nCommentParentIsreplymail=0;
+					$oHomefreshCommentParent=HomefreshcommentModel::F('homefreshcomment_id=?',$oHomefreshcomment->homefreshcomment_parentid)->query();
+
+					if($oHomefreshCommentParent['homefreshcomment_id']){
+						$bHaveParentcomment=true;
+					}
+				}
+
+				if($bHaveParentcomment===true){
+					$nCommentParentIsreplymail=$oHomefreshCommentParent->homefreshcomment_isreplymail;
+					$sCommentParentName=$oHomefreshCommentParent->homefreshcomment_name;
+					$sCommentParentContent=$oHomefreshCommentParent->homefreshcomment_content;
+					$sCommentParentLink=$GLOBALS['_option_']['site_url'].'/index.php?app=home&c=ucenter&a=view&id='.$oHomefreshCommentParent->homefresh_id.'&isolation_commentid='.$oHomefreshCommentParent['homefreshcomment_id'];
+					$sCommentParentEmail=$oHomefreshCommentParent->homefreshcomment_email;
+					$sCommentParentUrl=$oHomefreshCommentParent->homefreshcomment_url;
+				}else{
+					$nCommentParentIsreplymail=0;
+					$sCommentParentName=$sCommentParentContent=$sCommentParentLink=$sCommentParentEmail=$sCommentParentUrl='';
+				}
+				
+				Comment_Extend::commentSendmail($oHomefreshcomment['homefreshcomment_name'],$oHomefreshcomment['homefreshcomment_content'],$sCommentLink,$oHomefreshcomment['homefreshcomment_email'],$oHomefreshcomment['homefreshcomment_url'],$nCommentParentIsreplymail,$sCommentParentEmail,$sCommentParentName,$sCommentParentContent,$sCommentParentLink,$sCommentParentEmail,$sCommentParentUrl);
+			}catch(Exception $e){
+				$this->E($e->getMessage());
+			}
 		}
 
 		$arrCommentData=$oHomefreshcomment->toArray();
@@ -210,136 +231,6 @@ class AddcommentController extends GlobalchildController{
 		$this->A($arrCommentData,Dyhb::L('添加新鲜事评论成功','Controller/Homefresh'),1);
 	}
 
-	protected function send_cookie($oCommentModel){
-		Dyhb::cookie('the_comment_name',$oCommentModel->homefreshcomment_name,86400);
-		Dyhb::cookie('the_comment_url',$oCommentModel->homefreshcomment_url,86400);
-		Dyhb::cookie('the_comment_email',$oCommentModel->homefreshcomment_email,86400);
-	}
-
-	protected function comment_sendmail($oCommentModel){
-		$bSendMail=$bSendMailAdmin=$bSendMailAuthor=false;
-
-		// 是否发送邮件
-		$sAdminEmail=$GLOBALS['_option_']['admin_email'];
-		if($GLOBALS['_cache_']['home_option']['comment_mail_to_admin']==1 && !empty($sAdminEmail)){
-			$bSendMailAdmin=true;
-		}
-
-		if($GLOBALS['_cache_']['home_option']['comment_mail_to_author']==1 && !empty($oCommentModel->homefreshcomment_parentid)){
-			$bSendMailAuthor=true;
-		}
-
-		if($bSendMailAdmin || $bSendMailAuthor){
-			$bSendMail=true;
-		}
-
-		if($bSendMail===false){
-			return;
-		}
-
-		// 开始发送邮件
-		$oMail=Dyhb::instance('MailModel');
-		$oMailConnect=$oMail->getMailConnect();
-		if($bSendMailAdmin===true){
-			$sEmailSubject=$this->get_email_to_admin_subject($oCommentModel);
-			$sEmailMessage=$this->get_email_to_admin_message($oCommentModel,$oMailConnect);
-			$this->send_a_email($oMailConnect,$sAdminEmail,$sEmailSubject,$sEmailMessage);
-		}
-
-		if($bSendMailAuthor===true){
-			$oCommentParent=HomefreshcommentModel::F('homefreshcomment_id=?',$oCommentModel->homefreshcomment_parentid)->query();
-			if($oCommentParent->homefreshcomment_isreplymail==1 && !empty($oCommentParent->homefreshcomment_email)){
-				$sEmailSubject=$this->get_email_to_author_subject($oCommentParent);
-				$sEmailMessage=$this->get_email_to_author_message($oCommentParent,$oCommentModel,$oMailConnect);
-				$this->send_a_email($oMailConnect,$oCommentParent->homefreshcomment_email,$sEmailSubject,$sEmailMessage);
-			}
-		}
-
-		return;
-	}
-
-	protected function send_a_email($oMailConnect,$sEmailTo,$sEmailSubject,$sEmailMessage){
-		$oMail=Dyhb::instance('MailModel');
-		$oMail->sendAEmail($oMailConnect,$sEmailTo,$sEmailSubject,$sEmailMessage,'home');
-		if($oMail->isError()){
-			$this->E($oMail->getErrorMessage());
-		}
-	}
-
-	protected function get_email_to_admin_subject($oCommentModel){
-		return Dyhb::L('你的朋友【%s】在您的网站（%s）留言了！','Controller/Homefresh',null,$oCommentModel->homefreshcomment_name,$GLOBALS['_option_']['site_name']);
-	}
-
-	protected function get_email_to_admin_message($oCommentModel,$oMailConnect){
-		$sLine=$this->get_mail_line($oMailConnect);
-
-		$sMessage=$this->get_email_to_admin_subject($oCommentModel)."{$sLine}";
-		$sMessage.="-----------------------------------------------------{$sLine}";
-		$sMessage.=$oCommentModel->homefreshcomment_content."{$sLine}{$sLine}";
-		$sMessage.=Dyhb::L('请进入下面链接查看留言','Controller/Homefresh').":{$sLine}";
-		$sMessage.=$GLOBALS['_option_']['site_url'].'/index.php?app=home&c=ucenter&a=view&id='.$oCommentModel->homefresh_id.'&isolation_commentid='.$oCommentModel['homefreshcomment_id']."{$sLine}";
-		$sMessage.="-----------------------------------------------------{$sLine}";
-		$sMessage.=Dyhb::L('名字','Controller/Homefresh').':'.$oCommentModel->homefreshcomment_name."{$sLine}";
-
-		if(!empty($oCommentModel->homefreshcomment_email)){
-			$sMessage.='E-mail:'.$oCommentModel->homefreshcomment_email."{$sLine}";
-		}
-
-		if(!empty($oCommentModel->homefreshcomment_url)){
-			$sMessage.=Dyhb::L('主页','Controller/Homefresh').':'.$oCommentModel->homefreshcomment_url."{$sLine}";
-		}
-
-		$sMessage.="-----------------------------------------------------{$sLine}";
-		$sMessage.=Dyhb::L('消息来源','Controller/Homefresh').':'.$GLOBALS['_option_']['site_name']."{$sLine}";
-		$sMessage.=Dyhb::L('站点网址','Controller/Homefresh').':'.$GLOBALS['_option_']['site_url']."{$sLine}";
-		$sMessage.="-----------------------------------------------------{$sLine}";
-		$sMessage.=Dyhb::L('程序支持','Controller/Homefresh').':'.$GLOBALS['_option_']['windsforce_program_name']." " .WINDSFORCE_SERVER_VERSION. " Release " .WINDSFORCE_SERVER_RELEASE."{$sLine}";
-		$sMessage.=Dyhb::L('产品官网','Controller/Homefresh').':'.$GLOBALS['_option_']['windsforce_program_url']."{$sLine}";
-
-		return $sMessage;
-	}
-
-	protected function get_email_to_author_subject($oCommentModel){
-		return Dyhb::L("我的朋友：【%s】您在博客（%s）发表的评论被回复了！",'Controller/Homefresh',null,$oCommentModel->homefreshcomment_name,$GLOBALS['_option_']['site_name']);
-	}
-
-	protected function get_email_to_author_message($oCommentModel,$oCommentNew,$oMailSend){
-		$sLine=$this->get_mail_line($oMailSend);
-
-		$sMessage=$this->get_email_to_author_subject($oCommentModel).$sLine;
-		$sMessage.="-----------------------------------------------------{$sLine}";
-		$sMessage.=Dyhb::L('您的评论','Controller/Homefresh').":{$sLine}";
-		$sMessage.=$oCommentModel->homefreshcomment_content."{$sLine}";
-		$sMessage.="-----------------------------------------------------{$sLine}";
-		$sMessage.="【".$oCommentModel->homefreshcomment_name."】".Dyhb::L('回复说','Controller/Homefresh').":{$sLine}";
-		$sMessage.=$oCommentNew->homefreshcomment_content."{$sLine}{$sLine}";
-		$sMessage.=Dyhb::L('请进入下面链接查看留言','Controller/Homefresh').":{$sLine}";
-		$sMessage.=$GLOBALS['_option_']['site_url'].'/index.php?app=home&c=ucenter&a=view&id='.$oCommentModel->homefresh_id.'&isolation_commentid='.$oCommentModel['homefreshcomment_id']."{$sLine}";
-		$sMessage.="-----------------------------------------------------{$sLine}";
-		$sMessage.=Dyhb::L('名字','Controller/Homefresh').':'.$oCommentModel->homefreshcomment_name."{$sLine}";
-
-		if(!empty($oCommentModel->homefreshcomment_email)){
-			$sMessage.='E-mail:'.$oCommentModel->homefreshcomment_email."{$sLine}";
-		}
-
-		if(!empty($oCommentModel->homefreshcomment_url)){
-			$sMessage.=Dyhb::L('主页','Controller/Homefresh').':'.$oCommentModel->homefreshcomment_url."{$sLine}";
-		}
-
-		$sMessage.="-----------------------------------------------------{$sLine}";
-		$sMessage.=Dyhb::L('消息来源','Controller/Homefresh').':'.$GLOBALS['_option_']['site_name']."{$sLine}";
-		$sMessage.=Dyhb::L('站点网址','Controller/Homefresh').':'.$GLOBALS['_option_']['site_url']."{$sLine}";
-		$sMessage.="-----------------------------------------------------{$sLine}";
-		$sMessage.=Dyhb::L('程序支持','Controller/Homefresh').':'.$GLOBALS['_option_']['windsforce_program_name']." " .WINDSFORCE_SERVER_VERSION. " Release " .WINDSFORCE_SERVER_RELEASE."{$sLine}";
-		$sMessage.=Dyhb::L('产品官网','Controller/Homefresh').':'.$GLOBALS['_option_']['windsforce_program_url']."{$sLine}";
-
-		return $sMessage;
-	}
-
-	protected function get_mail_line($oMailConnect){
-		return $oMailConnect->getIsHtml()===true?'<br/>':"\r\n";
-	}
-	
 	protected function cache_site_(){
 		if(!Dyhb::classExists('Cache_Extend')){
 			require_once(Core_Extend::includeFile('function/Cache_Extend'));
--- upload/source/function/Comment_Extend.class.php
+++ upload/source/function/Comment_Extend.class.php
@@ -175,4 +175,158 @@ class Comment_Extend{
 		return true;
 	}
 
+	static public function sendCookie($sCommentname,$sCommenturl,$sCommentemail){
+		Dyhb::cookie('the_comment_name',$sCommentname,86400);
+		Dyhb::cookie('the_comment_url',$sCommenturl,86400);
+		Dyhb::cookie('the_comment_email',$sCommentemail,86400);
+	}
+
+	static public function addFeed($sTitle,$sType,$sCommentLink,$sCommentTitle,$sCommentMessage){
+		$sFeedtemplate='<div class="feed_'.$sType.'"><span class="feed_title">'.$sTitle.'&nbsp;<a href="{@commentlink}">{commenttitle}</a></span><div class="feed_content"><div class="feed_quote"><span class="feed_quoteinfo">{commentmessage}</span></div></div><div class="feed_action"><a href="{@commentlink}">'.Dyhb::L('回复','__COMMON_LANG__@Function/Comment_Extend').'</a></div></div>';
+
+		$arrFeeddata=array(
+			'@commentlink'=>$sCommentLink,
+			'commenttitle'=>G::subString($sCommentTitle,0,30),
+			'commentmessage'=>G::subString($sCommentMessage,0,100),
+		);
+
+		Core_Extend::addFeed($sFeedtemplate,$arrFeeddata);
+	}
+
+	static public function addNotice($sTitle,$sType,$sCommentLink,$sCommentTitle,$sCommentMessage,$nUserid,$sNoticeType,$nFromId){
+		$sNoticetemplate='<div class="notice_'.$sType.'"><span class="notice_title"><a href="{@space_link}">{user_name}</a>&nbsp;'.$sTitle.'&nbsp;<a href="{@commentlink}">{commenttitle}</a></span><div class="notice_content"><div class="notice_quote"><span class="notice_quoteinfo">{commentmessage}</span></div></div><div class="notice_action"><a href="{@commentlink}">'.Dyhb::L('查看','__COMMON_LANG__@Function/Comment_Extend').'</a></div></div>';
+
+		$arrNoticedata=array(
+			'@space_link'=>'home://space@?id='.$GLOBALS['___login___']['user_id'],
+			'user_name'=>$GLOBALS['___login___']['user_name'],
+			'@commentlink'=>$sCommentLink,
+			'commenttitle'=>G::subString($sCommentTitle,0,30),
+			'commentmessage'=>G::subString($sCommentMessage,0,100),
+		);
+
+		Core_Extend::addNotice($sNoticetemplate,$arrNoticedata,$nUserid,$sNoticeType,$nFromId);
+	}
+
+	static public function commentSendmail($sCommentName,$sCommentContent,$sCommentLink,$sCommentEmail,$sCommentUrl,$nCommentParentIsreplymail,$sCommentParentName,$sCommentParentContent,$sCommentParentLink,$sCommentParentEmail,$sCommentParentUrl){
+		$bSendMail=$bSendMailAdmin=$bSendMailAuthor=false;
+
+		// 是否发送邮件
+		$sAdminEmail=$GLOBALS['_option_']['admin_email'];
+		if($GLOBALS['_cache_']['home_option']['comment_mail_to_admin']==1 && !empty($sAdminEmail)){
+			$bSendMailAdmin=true;
+		}
+
+		if($GLOBALS['_cache_']['home_option']['comment_mail_to_author']==1 && !empty($nCommentParentid)){
+			$bSendMailAuthor=true;
+		}
+
+		if($bSendMailAdmin || $bSendMailAuthor){
+			$bSendMail=true;
+		}
+
+		if($bSendMail===false){
+			return;
+		}
+
+		// 开始发送邮件
+		$oMail=Dyhb::instance('MailModel');
+		$oMailConnect=$oMail->getMailConnect();
+		if($bSendMailAdmin===true){
+			$sEmailSubject=self::getEmailToAdminsubject($sCommentName);
+			$sEmailMessage=self::getEmailToAdminmessage($oMailConnect,$sCommentName,$sCommentContent,$sCommentLink,$sCommentEmail,$sCommentUrl);
+			self::sendAEmail($oMailConnect,$sAdminEmail,$sEmailSubject,$sEmailMessage);
+		}
+
+		if($bSendMailAuthor===true){
+			if($nCommentParentIsreplymail==1 && !empty($sCommentParentEmail)){
+				$sEmailSubject=self::getEmailToAuthorsubject($sCommentParentName);
+				$sEmailMessage=self::getEmailToAuthormessage($oMailConnect,$sCommentParentName,$sCommentParentContent,$sCommentParentLink,$sCommentParentEmail,$sCommentParentUrl,$sCommentContent);
+				self::sendAEmail($oMailConnect,$sCommentParentEmail,$sEmailSubject,$sEmailMessage);
+			}
+		}
+
+		return;
+	}
+
+	static public function getEmailToAdminsubject($sCommentName){
+		return Dyhb::L('你的朋友【%s】在您的网站（%s）留言了！','__COMMON_LANG__@Function/Comment_Extend',null,$sCommentName,$GLOBALS['_option_']['site_name']);
+	}
+
+	static public function getEmailToAdminmessage($oMailConnect,$sCommentName,$sCommentContent,$sCommentLink,$sCommentEmail,$sCommentUrl){
+		$sLine=self::getMailline($oMailConnect);
+
+		$sMessage=self::getEmailToAdminsubject($sCommentName)."{$sLine}";
+		$sMessage.="-----------------------------------------------------{$sLine}";
+		$sMessage.=$sCommentContent."{$sLine}{$sLine}";
+		$sMessage.=Dyhb::L('请进入下面超链接查看留言','__COMMON_LANG__@Function/Comment_Extend').":{$sLine}";
+		$sMessage.=$sCommentLink."{$sLine}";
+		$sMessage.="-----------------------------------------------------{$sLine}";
+		$sMessage.=Dyhb::L('名字','__COMMON_LANG__@Function/Comment_Extend').':'.$sCommentName."{$sLine}";
+
+		if(!empty($sCommentEmail)){
+			$sMessage.='E-mail:'.$sCommentEmail."{$sLine}";
+		}
+
+		if(!empty($sCommentUrl)){
+			$sMessage.=Dyhb::L('主页','__COMMON_LANG__@Function/Comment_Extend').':'.$sCommentUrl."{$sLine}";
+		}
+
+		$sMessage.=self::getSiteinf($sLine);
+
+		return $sMessage;
+	}
+
+	static function getEmailToAuthorsubject($sCommentName){
+		return Dyhb::L("我的朋友：【%s】您在网站（%s）发表的评论被回复了！",'__COMMON_LANG__@Function/Comment_Extend',null,$sCommentName,$GLOBALS['_option_']['site_name']);
+	}
+
+	protected function getEmailToAuthormessage($oMailSend,$sCommentName,$sCommentContent,$sCommentLink,$sCommentEmail,$sCommentUrl,$sNewCommentContent){
+		$sLine=self::getMailline($oMailSend);
+
+		$sMessage=self::getEmailToAuthorsubject($sCommentName).$sLine;
+		$sMessage.="-----------------------------------------------------{$sLine}";
+		$sMessage.=Dyhb::L('您的评论','__COMMON_LANG__@Function/Comment_Extend').":{$sLine}";
+		$sMessage.=$sCommentContent."{$sLine}";
+		$sMessage.="-----------------------------------------------------{$sLine}";
+		$sMessage.="【".$sCommentName."】".Dyhb::L('回复说','__COMMON_LANG__@Function/Comment_Extend').":{$sLine}";
+		$sMessage.=$sNewCommentContent."{$sLine}{$sLine}";
+		$sMessage.=Dyhb::L('请进入下面链接查看评论','__COMMON_LANG__@Function/Comment_Extend').":{$sLine}";
+		$sMessage.=$sCommentLink."{$sLine}";
+		$sMessage.="-----------------------------------------------------{$sLine}";
+		$sMessage.=Dyhb::L('名字','__COMMON_LANG__@Function/Comment_Extend').':'.$sCommentName."{$sLine}";
+
+		if(!empty($sCommentEmail)){
+			$sMessage.='E-mail:'.$sCommentEmail."{$sLine}";
+		}
+
+		if(!empty($sCommentUrl)){
+			$sMessage.=Dyhb::L('主页','__COMMON_LANG__@Function/Comment_Extend').':'.$sCommentUrl."{$sLine}";
+		}
+
+		$sMessage.=self::getSiteinf($sLine);
+
+		return $sMessage;
+	}
+
+	static public function getMailline($oMailConnect){
+		return $oMailConnect->getIsHtml()===true?'<br/>':"\r\n";
+	}
+
+	static public function getSiteinfo($sLine){
+		$sMessage="-----------------------------------------------------{$sLine}";
+		$sMessage.=Dyhb::L('消息来源','__COMMON_LANG__@Function/Comment_Extend').':'.$GLOBALS['_option_']['site_name']."{$sLine}";
+		$sMessage.=Dyhb::L('站点网址','__COMMON_LANG__@Function/Comment_Extend').':'.$GLOBALS['_option_']['site_url']."{$sLine}";
+		$sMessage.="-----------------------------------------------------{$sLine}";
+		$sMessage.=Dyhb::L('程序支持','__COMMON_LANG__@Function/Comment_Extend').':'.$GLOBALS['_option_']['windsforce_program_name']." " .WINDSFORCE_SERVER_VERSION. " Release " .WINDSFORCE_SERVER_RELEASE."{$sLine}";
+		$sMessage.=Dyhb::L('产品官网','__COMMON_LANG__@Function/Comment_Extend').':'.$GLOBALS['_option_']['windsforce_program_url']."{$sLine}";
+	}
+
+	static public function sendAEmail($oMailConnect,$sEmailTo,$sEmailSubject,$sEmailMessage){
+		$oMail=Dyhb::instance('MailModel');
+		$oMail->sendAEmail($oMailConnect,$sEmailTo,$sEmailSubject,$sEmailMessage,'home');
+		if($oMail->isError()){
+			Dyhb::E($oMail->getErrorMessage());
+		}
+	}
+
 }
