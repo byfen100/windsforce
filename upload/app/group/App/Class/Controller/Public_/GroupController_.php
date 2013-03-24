a8cb771c4fb2d5d4d7ad073361a425c466a48366
xiaoniuge <635750556@qq.com>
2013-03-24 17:33:07 +0800

    小组初始化

---
+++ upload/app/group/App/Class/Controller/Public_/GroupController_.php
@@ -0,0 +1,13 @@
+<?php
+/* [$WindsForce] (C)WindsForce TEAM Since 2012.03.17.
+   小组列表控制器($Liu.XiangMin)*/
+
+!defined('DYHB_PATH') && exit;
+
+class GroupController extends Controller{
+
+	public function index(){
+		$this->display('public+group');
+	}
+
+}
