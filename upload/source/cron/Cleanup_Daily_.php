d353e6ea9a5a6084c4762444471a14bd8ae6d1cf
小牛哥Dyhb <635750556@qq.com>
2013-05-06 14:38:40 +0800

    #1.0.1 将用户排行统计加入每日更新计划

--- upload/source/cron/Cleanup_Daily_.php
+++ upload/source/cron/Cleanup_Daily_.php
@@ -3,3 +3,11 @@
    今日数据清理（默认每日0时0分）($Liu.XiangMin)*/
 
 !defined('DYHB_PATH') && exit;
+
+/** 导入缓存组件 */
+if(!Dyhb::classExists('Cache_Extend')){
+	require_once(Core_Extend::includeFile('function/Cache_Extend'));
+}
+
+/** 每日更新用户排行数据 */
+Cache_Extend::updateCache('usertop');
